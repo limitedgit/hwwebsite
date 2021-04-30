5de3627ff22f49b080f655d51a7291386528b13e
Lucas Wung <59458611+tkoppop@users.noreply.github.com>
2021-04-30 16:04:20 -0700

    Simple js website stuff

---
+++ script.js
@@ -0,0 +1,13 @@
+const PEN ="p"
+const board = document.getElementById("board")
+const cpuGuessElement = document.getElementById("cpuG")
+const restartButton = document.getElementById("restartButton")
+const cpuGuessTextElement = document.querySelector("[data-cpu-guess-text]")
+
+
+startFunc()
+restartButton.addEventListener("click", startFunc)
+
+function startFunc(){
+    
+}
